075d7bb9c6fc268f61b05f770cea821dc30ce5c7
Nizar <nizarb@gmx.com>
2023-04-18 23:12:02 +0000

    added the files

---
+++ main.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import re, csv
+
+
+DRIVER_PATH = './chromedriver.exe'
+
+
+
+def intialize_driver():
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=chrome_options)
+    return driver
+
+
+def get_adress(driver, url):
+    driver.get(url)
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//h3[text()='Detailed Seller Information']/../.."))
+        )
+        
+        seller_info_box = driver.find_element(By.XPATH, "//h3[text()='Detailed Seller Information']/../..")
+        raw_text = seller_info_box.text
+        """split the raw text by 'Business Address:' and get the second element of the list"""
+        business_address = raw_text.split('Business Address:')[1]
+        business_address = business_address.replace('\n', ' ')
+        business_name = raw_text.split('Business Name:')[1].split('Business Address:')[0]
+        business_name = business_name.replace('Business Name: ', '')
+        business_name = business_name.replace('\n', ' ')
+        print('business name: ', business_name)
+        print('business address: ', business_address)
+        return driver, business_name, business_address      
+
+
+    except:
+        driver.quit()
+
+
+
+def utility():
+    # read sellers urls fron text file
+    with open('sellers.txt', 'r') as f:
+        urls = f.readlines()
+    # initialize driver
+    driver = intialize_driver()
+    # open csv file
+    with open('sellers.csv', 'w', newline='') as csvfile:
+        fieldnames = ['business_name', 'business_address']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for url in urls:
+            driver, business_name, business_address = get_adress(driver, url)
+            writer.writerow({'business_name': business_name, 'business_address': business_address})
+    driver.quit()
+
+if __name__ == '__main__':
+    utility()
+    
